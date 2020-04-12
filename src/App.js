561a170141e5666f3a56c7f330ae6dfcdca62131
hydroptix <samuelfrazee@hotmail.com>
2020-04-12 13:50:47 -0700

    Refactored starter code to allow table row deletion

--- src/App.js
+++ src/App.js
@@ -2,29 +2,48 @@ import React, { Component } from 'react'
 import Table from './Table'
 
 class App extends Component {
-  render() {
-    const characters = [
+  
+  //State is local and publicly accessible for every component
+  state = {
+    characters: [
       {
         name: 'Charlie',
-        job: 'Janitor',
+        job: 'Janitor'
       },
       {
         name: 'Mac',
-        job: 'Bouncer',
+        job: 'Bouncer'
       },
       {
         name: 'Dee',
-        job: 'Aspring actress',
+        job: 'Aspiring actress'
       },
       {
         name: 'Dennis',
-        job: 'Bartender',
-      },
+        job: 'Bartender'
+      }
     ]
+  }
+
+  //Still not super sure about this function declaration syntax, try with traditional parentheses
+  removeCharacter = index => {
+    const { characters } = this.state
+
+    //Create a new array, but if the index matches the index passed in, don't include it
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
+
+
+  render() {
+    const { characters } = this.state
 
     return (
       <div className="container">
-        <Table characterData={characters} />
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
       </div>
     )
   }
